a91929b9bc3c89ee57c8ccb6c07af0f94d618184
rlama7 <royalray7@gmail.com>
2020-08-26 17:19:03 -0700

    added server logic

---
+++ server.js
@@ -0,0 +1,46 @@
+/**
+ * server.js
+ *
+ */
+'use strict';
+
+const childProc = require('child_process');
+const CHILD_PROCESSES = 50;
+const URL = 'http://www.liamlama.com';
+
+(async () => {
+  let times = [];
+  let children = [];
+
+  for (let index = 0; index < CHILD_PROCESSES; index++) {
+    let childProcess = childProc.spawn('node', ['child.js', `--url=${URL}`]);
+    children.push(childProcess);
+  }
+
+  let responses = children.map(function wait(child) {
+    return new Promise(function c(res) {
+      child.stdout.on('data', (data) => {
+        console.log(`child stdout: ${data}`);
+        times.push(parseInt(data));
+      });
+      child.on('exit', function (code) {
+        if (code === 0) {
+          res(true);
+        } else {
+          res(false);
+        }
+      });
+    });
+  });
+
+  responses = await Promise.all(responses);
+
+  if (responses.filter(Boolean).length == responses.length) {
+    const sum = times.reduce((a, b) => a + b, 0);
+    const avg = sum / times.length || 0;
+    console.log(`average: ${avg}`);
+    console.log('success!');
+  } else {
+    console.log('failures');
+  }
+})();
